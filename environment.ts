b5f14bf70ffb0d14c1c2f320dccfa6ece0603cd0
Shubham <shubhz6599@gmail.com>
2025-11-25 11:15:55 +0530

    sds

--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export const environment:any = {
-  twilioBase: 'http://twilio.trialdemo.uk'
+  twilioBase: 'https://twilio.trialdemo.uk'
   // twilioBase: 'https://twillio-chatgpt-wrapper-production.up.railway.app'
   // twilioBase: 'http://localhost:3000'
 }
