61054deb73a9b2387ba7bcbf06941682932e065a
Shubham <shubhz6599@gmail.com>
2025-12-10 15:58:35 +0530

    Bugfix

--- environment.ts
+++ environment.ts
@@ -1,5 +1,6 @@
 export const environment:any = {
-  twilioBase: 'https://twilio.trialdemo.uk'
+  // twilioBase: 'https://twilio.trialdemo.uk'
   // twilioBase: 'https://twillio-chatgpt-wrapper-production.up.railway.app'
   // twilioBase: 'http://localhost:3000'
+  twilioBase: 'https://twillio-chatgpt-wrapper.onrender.com'
 }
